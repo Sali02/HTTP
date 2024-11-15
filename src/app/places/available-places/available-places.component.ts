40587eac3b600e9ec109f1b158b5ef040ca847ca
Sali's Code <siljjazi28@gmail.com>
2024-11-15 09:52:15 +0100

    Made an online course that connect the angular application with a backend

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,60 @@
+import { PlacesService } from './../places.service';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from "../places.component";
+
+
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesContainerComponent, PlacesComponent]
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+
+  isFetching = signal(false);
+  error = signal('');
+
+  private placesService = inject(PlacesService)
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+
+    this.isFetching.set(true);
+
+    const subscription =
+     this.placesService.loadAvailablePlaces().subscribe({
+       next: (places) => {
+         this.places.set(places)
+       },
+       error: (error: Error) => {
+         console.log(error);
+         this.error.set(error.message);
+       },
+       complete: () => {
+          this.isFetching.set(false);
+       }
+      });
+
+    this.destroyRef.onDestroy( () => {
+      subscription. unsubscribe(); 
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace)
+    .subscribe({
+      next: (resData) => console.log(resData)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+}
